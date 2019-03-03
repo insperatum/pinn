64c18fa188997311ef38f63929f572adec845755
Luke Hewitt <insperatum@gmail.com>
2019-03-02 23:44:05 -0500

    update for use as submodule

--- __init__.py
+++ __init__.py
@@ -1,3 +1,3 @@
-from pinn.robustfill import *
-from pinn.image_robustfill import Image_RobustFill
-from pinn.syntax_robustfill import SyntaxCheckingRobustFill
+from .robustfill import *
+from .image_robustfill import Image_RobustFill
+from .syntax_robustfill import SyntaxCheckingRobustFill
